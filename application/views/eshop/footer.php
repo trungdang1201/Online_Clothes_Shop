062e84d117d2b619fea26f42a5773544a2ee3293
Tung Lam <daongtunglam1611@gmail.com>
2022-07-20 09:11:36 +0700

    Fix admin css

--- application/views/eshop/footer.php
+++ application/views/eshop/footer.php
@@ -87,9 +87,5 @@
 	
     <script src="<?php echo ASSETS . THEME?>js/jquery.js"></script>
 	<script src="<?php echo ASSETS . THEME?>js/bootstrap.min.js"></script>
-	<script src="<?php echo ASSETS . THEME?>js/jquery.scrollUp.min.js"></script>
-	<script src="<?php echo ASSETS . THEME?>js/price-range.js"></script>
-    <script src="<?php echo ASSETS . THEME?>js/jquery.prettyPhoto.js"></script>
-    <script src="<?php echo ASSETS . THEME?>js/main.js"></script>
 </body>
 </html>
